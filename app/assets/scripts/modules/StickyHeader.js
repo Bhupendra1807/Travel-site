a07f43b5241b6b79fd18ff70296e84814e8f8c08
bhupendra <bhupendra1807@gmail.com>
2018-06-04 14:05:16 +0530

    completed lazy loading

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -3,6 +3,7 @@ import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoi
 import smoothScroll from 'jquery-smooth-scroll';
 class StickyHeader{
   constructor(){
+    this.lazyImages=$('.lazyload');
     this.siteHeader=$('.site-header');
     this.headerTriggerElement=$('.large-hero__title');
     this.createHeaderWaypoints();
@@ -10,6 +11,12 @@ class StickyHeader{
     this.headerLinks=$('.primary-nav a');
     this.createPageSectionWayPoints();
     this.addSmoothScrolling();
+    this.refresWayPoints();
+  }
+  refresWayPoints(){
+    this.lazyImages.on('load', function(){
+      Waypoints.refreshAll();
+    });
   }
 
   addSmoothScrolling(){
